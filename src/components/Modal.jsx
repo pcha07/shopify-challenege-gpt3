a1102d1772a8143a067bca66633a53c932da5066
Prabh Chahal <chahalprabh10@gmail.com>
2022-05-19 14:21:01 -0700

    final build

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,7 +7,6 @@ const Modal = () => {
   const [viewResultItems, setViewResultItems] = useRecoilState(resultItems);
   // if there is an error, set error message
   const [errorMsg, setErrorMsg] = useState("");
-  const secretKey = "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"
 
   // preset data, prompt is updated by the user
   const [data, setData] = useState({
@@ -33,7 +32,7 @@ const Modal = () => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${secretKey}`,
+          Authorization: `Bearer ${process.env.REACT_APP_OPENAI_SECRET}`,
           mode: "no-cors"
         },
         body: JSON.stringify(data),
