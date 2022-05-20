4dcf94f7c62d0b6c2cb7e875220d63c1790dc1f1
Prabh Chahal <chahalprabh10@gmail.com>
2022-05-19 18:15:16 -0700

    final build

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { FaRobot } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <header className="fixed top-0 z-50 w-full flex items-center bg-[#5D3FD3] px-2 py-1 justify-between">
+    <header className="fixed top-0 z-50 w-full flex items-center bg-[#5D3FD3] px-4 py-1 justify-between">
       <div className="p-2.5 transition ease-in-out hover:scale-105 cursor-pointer animate-pulse text-white">
         <a href="/">
           <FaRobot size={45} />
